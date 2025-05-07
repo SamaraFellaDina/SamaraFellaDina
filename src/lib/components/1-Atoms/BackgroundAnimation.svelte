b69428316fa38787c490cbfdf2c7b52c4f700970
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-05-07 16:11:23 +0200

    adding component and testing it on the page
    #4 to make sure it works. Will style it soon

---
+++ src/lib/components/1-Atoms/BackgroundAnimation.svelte
@@ -0,0 +1,14 @@
+
+<section>
+  <h1>Samara Fella Dina.</h1>
+  <p>hi! Welcome to my portfolio</p>
+</section>
+
+<style>
+  
+ section {
+  height: 100vh;
+  background-color: yellow;
+ }
+
+</style>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,2 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as BackgroundAnimation } from './components/1-Atoms/BackgroundAnimation.svelte';
