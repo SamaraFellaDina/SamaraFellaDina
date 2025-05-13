75bbf962179fc28a787c01f5b1fac0c0dcba85a7
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-05-13 15:58:44 +0200

    making a first animation
    I also added some context for some code, will remove later

--- src/lib/components/1-Atoms/BackgroundAnimation.svelte
+++ src/lib/components/1-Atoms/BackgroundAnimation.svelte
@@ -1,27 +1,83 @@
 <script>
-import { gsap } from "gsap";
+  // folling video: https://www.youtube.com/watch?v=L1afzNAhI40
+  // import gsap
+  import { gsap } from "gsap";
+  import { SplitText } from "gsap/SplitText";
+  import { onMount } from "svelte";
+
+    // import plugin from gsap
+  gsap.registerPlugin(SplitText);
+
+  onMount(() => {
+    // This will split the text into lines
+    let split = SplitText.create(".logo", {
+      type: "chars",
+    });
+
+
+    gsap.from(split.chars, {
+      x:1000,
+      duration: 3,
+      stagger: 0.2,
+      opacity: 0,
+      ease: "elastic.out(1,1)",
+      stagger : {
+        amount: 0.5,
+        from: "random",
+      },
+    })
+  });
 
 
 
 </script>
-<section>
-  <h1>Samara Fella Dina.</h1>
-  <p>hi! Welcome to my portfolio</p>
-</section>
+
+<div>
+  <section>
+    <h1 class="logo">
+      <span>Samara</span>
+      <span>Fella</span>
+      <span>Dina.</span>
+    </h1>
+    <p>hi! Welcome to my portfolio</p>
+  </section>
+</div>
 
 <style>
-  section {
-    height: 100vh;
+
+  div {
     background-color: yellow;
+    height: 100vh;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+  }
+
+  section {
     display: grid;
     justify-content: center;
     align-items: end;
-    padding: 1rem;
+    width: 80vw;
+    height: 100%;
   }
 
   h1 {
-    font-size: 4rem;
+    font-size: 5rem;
     color: #000;
     text-transform: uppercase;
+
+    display: flex;
+    flex-direction: column;
+
+
+    @media (min-width: 500px) {
+      font-size: 5rem;
+    } 
+
+    @media (min-width: 1024px) {
+      font-size: 10rem;
+    }
   }
 </style>
