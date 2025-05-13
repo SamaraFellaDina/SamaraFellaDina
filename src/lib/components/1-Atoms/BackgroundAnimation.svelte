27e3215871d4ac6b7489deac520a0f0d311cd173
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-05-13 13:26:11 +0200

    styling homepage

--- src/lib/components/1-Atoms/BackgroundAnimation.svelte
+++ src/lib/components/1-Atoms/BackgroundAnimation.svelte
@@ -1,14 +1,24 @@
+<script>
 
+</script>
 <section>
   <h1>Samara Fella Dina.</h1>
   <p>hi! Welcome to my portfolio</p>
 </section>
 
 <style>
-  
- section {
-  height: 100vh;
-  background-color: yellow;
- }
+  section {
+    height: 100vh;
+    background-color: yellow;
+    display: grid;
+    justify-content: center;
+    align-items: end;
+    padding: 1rem;
+  }
 
+  h1 {
+    font-size: 4rem;
+    color: #000;
+    text-transform: uppercase;
+  }
 </style>
