e21d9f79727861038d23945ba51d01e9dbda2ea5
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-06-10 15:41:54 +0200

    added a layout.svelter

---
+++ src/lib/components/1-Atoms/Header.svelte
@@ -0,0 +1,16 @@
+
+<header>
+  <h1>Welcome to My Website</h1>
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Header } from "$lib/index.js";
+    /**
+     * @typedef {Object} Props
+     * @property {import('svelte').Snippet} [children]
+     */
+
+    /** @type {Props} */
+    let { children } = $props();
+</script>
+
+<Header />
+
+<main>{@render children?.()}</main>
+
+
