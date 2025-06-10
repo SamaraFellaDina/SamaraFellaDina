c67ea013a8a55d0b75fef665b9b7572af4567853
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-06-10 15:36:07 +0200

    added a header component

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as BackgroundAnimation } from './components/1-Atoms/BackgroundAnimation.svelte';
+export { default as Header } from './components/1-Atoms/Header.svelte';
