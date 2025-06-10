5dc0b50e9bfa29138999a6d5ababb245b653cf74
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-06-10 15:57:31 +0200

    add styling for the mobile
    This is the beginning for the mobile header, later I will add the other styling

--- src/lib/components/1-Atoms/Header.svelte
+++ src/lib/components/1-Atoms/Header.svelte
@@ -1,16 +1,43 @@
 
 <header>
-  <h1>Welcome to My Website</h1>
   <nav>
+    <h2>menu</h2>
     <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/contact">Contact</a></li>
+      <li><a href="/">Home.</a></li>
+      <li><a href="/about">About.</a></li>
+      <li><a href="/work">Work.</a></li>
+      <li><a href="/contact">Contact.</a></li>
     </ul>
   </nav>
 </header>
 
 <style>
+  header {
+  position: relative;
+  }
+nav {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column-reverse;
+}
+
+ul {
+  --header-size: 10rem;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   
+  background-color: black;
+  border-radius: 1000em;
+  width: var(--header-size);
+  height: var(--header-size);
+}
+
 </style>
