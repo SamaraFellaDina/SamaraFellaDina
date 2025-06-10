7ca6288d4c12fe78fa690ec5de2b7281fbe986dc
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-06-10 16:39:07 +0200

    added style for state
    There is a class added where it styles the checked state

--- src/lib/components/1-Atoms/Header.svelte
+++ src/lib/components/1-Atoms/Header.svelte
@@ -1,5 +1,5 @@
 
-<header>
+<header class='clicked'>
   <nav>
     <h2>menu</h2>
     <ul>
@@ -7,6 +7,7 @@
       <li><a href="/about">About.</a></li>
       <li><a href="/work">Work.</a></li>
       <li><a href="/contact">Contact.</a></li>
+      <li><a href='/'>Close.</a></li>
     </ul>
   </nav>
 </header>
@@ -20,35 +21,64 @@
     padding: 1rem;
     justify-content: center;
     align-items: flex-end;
+    z-index: 1000;
   }
 
   nav {
-  display: flex;
-  flex-direction: column-reverse;
-  align-items: center;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+    gap: .5rem;
 }
 
 ul {
-  --header-size: 10rem;
-  list-style: none;
-  padding: 0;
-  margin: 0;
+    --header-size: 5rem;
+    list-style: none;
+    padding: 0;
+    margin: 0;
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
-  mix-blend-mode: difference;
-  background-color: white;
-  border-radius: 1000em;
-  width: var(--header-size);
-  height: var(--header-size);
-  
+    mix-blend-mode: difference;
+    background-color: white;
+    border-radius: 1000em;
+    width: var(--header-size);
+    height: var(--header-size);
+    
 }
 
 li {
   visibility: hidden;
+} 
+
+h2 {
+  margin:0;
+  background-color: var(--link-color);
+}
+
+.clicked {
+  align-items: center;
+  padding: 0;
+}
+.clicked nav {
+  justify-content: center;
+}
+.clicked ul {
+  --header-size: 100vw;
+  justify-content: space-between;
+  padding: 3rem;
+}
+.clicked li {
+  visibility: visible;
+  font-size: 1.5rem;
+  transition: var(--hover-transition);
+
+  &:hover {
+    transform: scale(1.1);
+  }
 }
 
 </style>
