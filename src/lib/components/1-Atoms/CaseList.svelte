c73f9c3a1f7a90b5ad7a530bed681b5a3f1a49f2
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-11-27 12:12:43 +0100

    create a list item

--- src/lib/components/1-Atoms/CaseList.svelte
+++ src/lib/components/1-Atoms/CaseList.svelte
@@ -1,1 +1,17 @@
+<script>
+  export let cases = [];
+  const listOfCases = cases?.[0]?.listOfCases ?? [];
 
+</script>
+
+<ul>
+  {#each listOfCases as caseItem}
+    <li>
+      <a href='/'>
+        <h2>{caseItem.caseTitle}</h2>
+        <p>{caseItem.date}</p>
+        <p>{caseItem.location}</p>
+      </a>
+    </li>
+  {/each}
+</ul>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,7 +1,10 @@
 <script>
+  import { CaseList } from '$lib/index.js';
   export let data
 </script>
 <h1>
   Portfolio Page
 </h1>
+
+<CaseList cases={data.cases} />
 
