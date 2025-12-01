02eba26a3f1a4019aec5f4b37e2b804903d4ad90
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-01 17:47:54 +0100

    added style to hero
     I added a basic style to the component for now, but i'll want to add the styling for if there is a image present

--- src/lib/components/1-Atoms/Hero.svelte
+++ src/lib/components/1-Atoms/Hero.svelte
@@ -1,5 +1,33 @@
 <script>
-  export let heroTitle ='Hero';
+  export let heroTitle ='input hero title here!';
+  export let thumbmail;
 </script>
 
+<div>
+
+
+{#if thumbmail}
+  <img alt="thumbmail" href="{thumbmail}"/>
+{/if}
+
 <h1>{heroTitle}</h1>
+
+</div>
+
+<style>
+  div {
+    height: 50dvh;
+    display: flex;
+    flex-direction:column;
+    justify-content:flex-end
+  }
+
+  div h1 {
+    color: var(--link-color);
+    font-style:italic;
+    letter-spacing: .2rem;
+    text-transform:uppercase;
+    font-size: 5rem;
+    font-weight: 900;
+  }
+</style>
