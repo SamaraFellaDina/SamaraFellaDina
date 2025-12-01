444355987d479405057965634ebf7da5716fa63a
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-01 18:00:33 +0100

    adding styles to component

--- src/lib/components/1-Atoms/Hero.svelte
+++ src/lib/components/1-Atoms/Hero.svelte
@@ -1,6 +1,6 @@
 <script>
   export let heroTitle ='input hero title here!';
-  export let thumbmail;
+  export let thumbmail = "https://media.tenor.com/wDrtIwh5_ckAAAAM/lebron-james-mickey-mouse.gif";
 </script>
 
 <div>
@@ -19,15 +19,24 @@
     height: 50dvh;
     display: flex;
     flex-direction:column;
-    justify-content:flex-end
-  }
+    justify-content:flex-end;
 
-  div h1 {
+    & img {
+      height:40dvh;
+      width:100%;
+      object-fit:cover;
+      box-shadow: inset 0 0 10px red;
+      
+    }
+
+    & h1 {
     color: var(--link-color);
     font-style:italic;
     letter-spacing: .2rem;
     text-transform:uppercase;
     font-size: 5rem;
     font-weight: 900;
+    margin-block:0;
+    }
   }
 </style>
