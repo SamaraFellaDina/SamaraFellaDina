6a87b31507078b763528c863bbccc226f31dec95
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-01 20:32:28 +0100

    add image to list

--- src/lib/components/1-Atoms/CaseList.svelte
+++ src/lib/components/1-Atoms/CaseList.svelte
@@ -1,6 +1,7 @@
 <script>
   export let cases = [];
   const listOfCases = cases?.[0]?.listOfCases ?? [];
+  const fallbackImage = "https://preview.redd.it/random-question-but-does-anyone-have-versions-of-this-cat-v0-ya8qikz9kn0f1.png?auto=webp&s=c2fdba9a3904ab3bec9e7367e380f66343c2929a"
 
 </script>
 
@@ -14,12 +15,11 @@
         <p>{caseItem.location}</p>
 
         <img 
-        src="{caseItem.thumbnail?.url}"
+        src="{caseItem.thumbnail?.url || fallbackImage}"
         alt="thumbnail"
         width="{caseItem.thumbnail?.width}"
         height="{caseItem.thumbnail?.height}"
         />
-
       </a>
     </li>
   {/each}
@@ -31,4 +31,11 @@
     margin-block:0;
     padding-inline:0;
   }
+
+  img {
+    width: 100%;
+    height: 15rem;
+    object-fit: cover;
+  }
+
 </style>
