98b07407e183437027ae305fdc4ce15b1bb03c71
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-01 20:28:19 +0100

    added images

--- src/lib/components/1-Atoms/CaseList.svelte
+++ src/lib/components/1-Atoms/CaseList.svelte
@@ -6,12 +6,29 @@
 
 <ul>
   {#each listOfCases as caseItem}
+  {console.log(caseItem)}
     <li>
       <a href="/portfolio/{caseItem.slug}">
         <h2>{caseItem.caseTitle}</h2>
         <p>{caseItem.date}</p>
         <p>{caseItem.location}</p>
+
+        <img 
+        src="{caseItem.thumbnail?.url}"
+        alt="thumbnail"
+        width="{caseItem.thumbnail?.width}"
+        height="{caseItem.thumbnail?.height}"
+        />
+
       </a>
     </li>
   {/each}
 </ul>
+
+<style>
+  ul {
+    list-style:none;
+    margin-block:0;
+    padding-inline:0;
+  }
+</style>
