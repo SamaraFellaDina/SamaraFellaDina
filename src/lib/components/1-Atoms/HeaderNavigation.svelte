0cfdf4460b00e48f48f32ac22f894b65aa189032
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-05 17:27:03 +0100

    remove comment

--- src/lib/components/1-Atoms/HeaderNavigation.svelte
+++ src/lib/components/1-Atoms/HeaderNavigation.svelte
@@ -40,7 +40,6 @@ nav ul li a {
   display: flex;
   width:fit-content;
 
-
   &:hover {
     background-color: var(--link-color-active); 
     color: hsl(56.26deg 100% 50%);
@@ -73,97 +72,3 @@ nav ul li a {
 
 
   </style>
-
-
-
-  <!-- <style>
-a {
-  z-index: 1;
-    &:visited {
-      color :var(--text-color-dark);
-  }
-
-  &:hover{
-      color :var(--text-color-dark);
-    text-decoration:none;
-  }
-  
-  &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: var(--link-color); 
-    z-index: -2; 
-  }
-
-  &::after {
-    content: "";
-    color:var(--text-color-light);
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 0%;
-    height: 100%;
-    background-color: var(--link-color-active); 
-    transition: var(--hover-transition);
-    z-index: -1; 
-  }
-
-  &:hover::after {
-    width: 100%;
-  }
-
-  &:hover {
-    color: var(--text-color-light);
-    transition-delay: 100ms;
-  }
-}
-
-
-
-
-
-.default ul,
-.title ul {
-  list-style: none;
-  z-index: 2;
-  margin-block:0;
-  padding-inline:0;
-}
-
-.title {
-  display: block;
-  position: relative;
-  z-index: 3;
-  font-size: 2rem;
-}
-
-.default {
-  height: 100%;
-
-  &  ul {
-    display: flex;
-    justify-content: flex-end;
-    height: 100%
-
-  }
-
-  &  ul li {
-    height: 100%;
-    cursor: pointer;
-  }
-
-  &  ul li a {
-  display: flex;        
-  align-items: center; 
-  height: 100%;
-  padding: 0 1rem;
-  }
-}
-
-
-
-  </style> -->
