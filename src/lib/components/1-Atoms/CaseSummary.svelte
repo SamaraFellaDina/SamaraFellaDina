2fb13f685113c79e220503c9517fc43ae6789934
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-15 11:30:46 +0100

    add border to summary

--- src/lib/components/1-Atoms/CaseSummary.svelte
+++ src/lib/components/1-Atoms/CaseSummary.svelte
@@ -12,3 +12,15 @@
       
     {/each}
   </section>
+
+<style> 
+section details {
+  --details-border: 1px solid var(--link-color);
+	border-top: var(--details-border);
+	border-bottom: var(--details-border);
+	border-left: none;
+	border-right: none;
+  padding: 1.5rem;
+
+}
+</style>
