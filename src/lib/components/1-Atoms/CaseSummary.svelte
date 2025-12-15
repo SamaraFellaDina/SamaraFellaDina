352b74a9aecf110e52212454f595d719e6efa58e
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-15 11:46:26 +0100

    add styling into summary

--- src/lib/components/1-Atoms/CaseSummary.svelte
+++ src/lib/components/1-Atoms/CaseSummary.svelte
@@ -17,10 +17,29 @@
 section details {
   --details-border: 1px solid var(--link-color);
 	border-top: var(--details-border);
-	border-bottom: var(--details-border);
 	border-left: none;
 	border-right: none;
   padding: 1.5rem;
 
+  &:last-child {
+    border-bottom: var(--details-border);
+  }
+
+  &:not([open]) {
+    cursor:pointer;
+  }
 }
+
+section details summary {
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+  list-style: none;
+  transition: var(--transition);
+  font-size: 2rem;
+}
+[open] summary {
+  padding: 1em 0em;
+}
+
 </style>
