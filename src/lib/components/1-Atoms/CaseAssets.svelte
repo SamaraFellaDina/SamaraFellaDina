1b14b9bca8a6c8a2a2d0cac3a639b787b977464f
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-16 16:15:34 +0100

    styling the cases

--- src/lib/components/1-Atoms/CaseAssets.svelte
+++ src/lib/components/1-Atoms/CaseAssets.svelte
@@ -4,8 +4,11 @@
 </script>
 
 
-<Wrapper>
+  <ul>
+
+
   {#each assets as asset}
+  <li>
     {#if asset.mimeType.startsWith('image/')}
       <img 
         src={asset.url} 
@@ -26,14 +29,53 @@
       <source src={asset.url} type="video/mp4" />
     </video>
     {/if}
+  </li>
+
   {/each}
-</Wrapper>
+    </ul>
+
 
 
 <style>
-  img, video {
+
+  ul { 
+    padding-inline: 0;
+    list-style: none;
+    margin-block: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 10rem;
+  }
+
+  ul li {
+    --default-space: 4rem;
+  }
+
+  ul li:nth-child(odd) {
+    padding-right: var(--default-space);
+
+    & img, 
+    video {
+      border-radius: 0 var(--border-radius) var(--border-radius) 0;
+    }
+  }
+
+  ul li:nth-child(even) {
+    padding-left: var(--default-space);
+    & img, 
+    video {
+      border-radius: var(--border-radius) 0 0 var(--border-radius);
+    }
+  }
+
+  ul li img, 
+  ul li video {
     width: 100%;
     height: auto;
+    max-height:80dvh;
     object-fit: cover;
+    border-radius: 0;
+
   }
 </style>
+
