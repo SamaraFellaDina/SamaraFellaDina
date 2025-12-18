04f35ac627d61dfb716c4ca2c2c7ae681ac9df51
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-18 22:18:01 +0100

    restructure the `index.js`
    to be more clear

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,19 +4,23 @@
 export { default as BackgroundAnimation } from './components/1-Atoms/BackgroundAnimation.svelte';
 export { default as HeaderNavigation } from './components/1-Atoms/HeaderNavigation.svelte';
 export { default as CaseList } from './components/1-Atoms/CaseList.svelte';
-export { default as CaseSummary } from './components/3-Organism/CaseSummary.svelte';
-export { default as CaseAssets } from './components/2-Molecules/CaseAssets.svelte';
 export { default as MarkDownContent } from './components/1-Atoms/MarkDownContent.svelte';
-export { default as Summary } from './components/2-Molecules/Summary.svelte';
-export { default as Hero } from './components/3-Organism/Hero.svelte';
 export { default as Icons } from './components/1-Atoms/Icons.svelte';
-export { default as References } from './components/3-Organism/References.svelte';
 export { default as Wrapper } from './components/1-Atoms/Wrapper.svelte';
 export { default as Tag } from './components/1-Atoms/Tag.svelte';
 
 // Molenules
-export { default as HomepageTitle } from './components/3-Organism/HomepageTitle.svelte';
 export { default as Header } from './components/2-Molecules/Header.svelte';
+export { default as CaseAssets } from './components/2-Molecules/CaseAssets.svelte';
+export { default as Summary } from './components/2-Molecules/Summary.svelte';
+
+// Organism
+export { default as CaseSummary } from './components/3-Organism/CaseSummary.svelte';
+export { default as Hero } from './components/3-Organism/Hero.svelte';
+export { default as References } from './components/3-Organism/References.svelte';
+export { default as HomepageTitle } from './components/3-Organism/HomepageTitle.svelte';
+
+// Templates
 
 
 // Pages
