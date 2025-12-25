373044c4ce57564a1f777b6d4c4aba25e06d5af9
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-25 16:33:37 +0100

    add styling to +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,27 @@
 <script>
+  import { Wrapper } from '$lib/index.js'
   import { error } from '@sveltejs/kit';
 	import { page } from '$app/state';
+  const gif = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2l4bzFlNjcxdjI2endvN3g0MHc0MDNrYnFxa2FhazhjajcyZG9oMSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/LgR0AnXJBrO4E/giphy.gif'
 </script>
 
-<section>
+<Wrapper backgroundColor='var(--link-color-active)'>
+  <section>
   <h1>{page.status} | {page.error.message}</h1>
-  <p>It seems you're a little lost.</p>
-  <a href='./'>Home.</a>
-</section>
+  <p>It seems like you're a little lost.</p>
+  <img src={gif} alt='oops there has a error occured'/>
+  <a href='/'>get me Home! :(</a>
+  </section>
+</Wrapper>
+
+<style>
+  section{
+    min-height: 90dvh;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items: center;
+  }
+</style>
+
 
