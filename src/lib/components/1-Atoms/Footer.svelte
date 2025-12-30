51f1d04cd21b7633ac3afaf1169ac79d1e9d32aa
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-12-30 17:10:15 +0100

    added footer as a component
     footer is added as a +layout.svelte. Added some dummy content for now that needs to be worked out

---
+++ src/lib/components/1-Atoms/Footer.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { Wrapper } from '$lib/index.js'
+</script>
+
+<Wrapper backgroundColor='var(--color-secondary)'>
+<section>
+  <h2>Contact</h2>
+  <p>lets meet! :)</p>
+</section>
+
+<section>
+  <h2>Socials</h2>
+  <ul>
+    <li>
+      <a href='/'>Instagram</a>
+    </li>
+      <li>
+      <a href='/'>Github</a>
+    </li>
+      <li>
+      <a href='/'>LinkedIn</a>
+    </li>
+  </ul>
+</section>
+
+<section>
+  <h2>Github</h2>
+  <ul>
+    <li>
+      <a href='/'>Bug report</a>
+    </li>
+        <li>
+      <a href='/'>Repository</a>
+    </li>
+        <li>
+      <a href='/'>Help this site improve :D</a>
+    </li>
+  </ul>
+</section>
+</Wrapper>
+
+
+<style>
+  section {
+    color: var(--color-tertriary);
+  }
+</style>
