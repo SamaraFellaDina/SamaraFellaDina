816e72dcc2162967cb79e592eeb507142bf731aa
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-01-06 12:38:06 +0100

    move about be hero content to separate component

---
+++ src/lib/components/1-Atoms/AboutMeHero.svelte
@@ -0,0 +1,52 @@
+<script>
+
+  export let heroPicture = "https://media.licdn.com/dms/image/v2/D4E03AQEzWc5ZoCy0UA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1719780890071?e=1769040000&v=beta&t=GEtJJA1tcyD4SsxJ5iaFGkWdOBgCupS8FBR25HFG4js"
+
+</script>
+
+<section>
+  <h1>
+    <span class="title">Hello! I'm </span>
+    <span class="name">sammm.my</span>
+  </h1>
+  <img alt="hero" src={heroPicture}>
+</section>
+
+<style>
+  section {
+    display: grid;
+    grid-template-areas:
+      "title"
+      "image"
+      "name";
+    justify-items: center;
+    gap:var(--gap-regular);
+    height:60dvh;
+  }
+
+  section img {
+    border-radius:100rem;
+    height:auto;
+    width:auto;
+    grid-area: image;
+  }
+
+  section h1 {
+  letter-spacing: .2rem;
+  font-size: 5rem;
+  font-weight: var(--font-weight-bold);
+  margin-block:0;
+  line-height: 4.5rem;
+  display: contents; 
+  }
+
+  .name {
+    grid-area: name;
+  }
+
+  .title {
+    grid-area: title;
+    font-size:3rem;
+  }
+
+</style>
--- src/lib/components/5-Pages/AboutMe.svelte
+++ src/lib/components/5-Pages/AboutMe.svelte
@@ -1,24 +1,10 @@
 <script>
   import { 
-    Hero, 
+    AboutMeHero,
     Wrapper 
     } from '$lib/index.js'
 </script>
 
 <Wrapper>
-  <section>
-    <h1>Hello! I'm Sammm.my</h1>
-    <img alt="hero" src="https://media.licdn.com/dms/image/v2/D4E03AQEzWc5ZoCy0UA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1719780890071?e=1769040000&v=beta&t=GEtJJA1tcyD4SsxJ5iaFGkWdOBgCupS8FBR25HFG4js">
-  </section>
+  <AboutMeHero /> 
 </Wrapper>
-
-  
-
-
-<style>
-  img {
-    border-radius:100rem;
-    height:60dvh;
-    width:auto;
-  }
-</style>
