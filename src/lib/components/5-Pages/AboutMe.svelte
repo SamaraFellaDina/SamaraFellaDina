1b1a9dc5d3a29f938a40a05c48192fc99080bb18
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-01-06 11:49:32 +0100

    add styling to about me compoennt

--- src/lib/components/5-Pages/AboutMe.svelte
+++ src/lib/components/5-Pages/AboutMe.svelte
@@ -6,5 +6,19 @@
 </script>
 
 <Wrapper>
-  <Hero heroTitle="About me"/>
+  <section>
+    <h1>Hello! I'm Sammm.my</h1>
+    <img alt="hero" src="https://media.licdn.com/dms/image/v2/D4E03AQEzWc5ZoCy0UA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1719780890071?e=1769040000&v=beta&t=GEtJJA1tcyD4SsxJ5iaFGkWdOBgCupS8FBR25HFG4js">
+  </section>
 </Wrapper>
+
+  
+
+
+<style>
+  img {
+    border-radius:100rem;
+    height:60dvh;
+    width:auto;
+  }
+</style>
