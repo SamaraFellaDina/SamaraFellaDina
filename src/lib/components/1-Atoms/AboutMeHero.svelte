a47853dcbae5a812752ea4dd6d9723c0fa27494a
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-01-09 13:59:26 +0100

    changed composition at the intro of the about me
    the different pictured could be better placed around the page instead putting them all together. So now they are more divided

--- src/lib/components/1-Atoms/AboutMeHero.svelte
+++ src/lib/components/1-Atoms/AboutMeHero.svelte
@@ -8,6 +8,7 @@
   } from '$lib/index.js'
 export let paragraph = 'no paragraph available'
 export let heroImages;
+let placeholder = "https://plus.unsplash.com/premium_photo-1746637466037-001842a48d31?q=80&w=1567&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
 </script>
 
 <Wrapper>
@@ -20,4 +21,45 @@ export let heroImages;
 
   <Paragraph content={paragraph}/>
   
+<Wrapper>
+    <ul>
+      <li> <img src={placeholder}></li>
+      <li> <img src={placeholder}></li>
+    </ul>
+</Wrapper>
 
+
+    <style>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+  @media(min-width:700px) {
+    display: flex;
+    flex-direction: row;
+    gap: var(--gap-regular);
+    align-items:center;
+  }
+}
+
+ ul li {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;   
+  aspect-ratio:1/1;
+}
+
+ul li:nth-child(2) {
+  display:none;
+
+  @media(min-width:700px) {
+    display:block;
+  }
+}
+
+ ul li img {
+  width: 100%;
+  height: 100%;
+}
+    </style>
