0f49df94114c1668b38833c063158bd28bcb5f1a
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-01-12 15:38:07 +0100

    update the layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,19 +4,14 @@
       Header,
       Footer
     } from "$lib/index.js";
-    /**
-     * @typedef {Object} Props
-     * @property {import('svelte').Snippet} [children]
-     */
-
-    /** @type {Props} */
     let { children } = $props();
 
 </script>
 
 <div>
     <Header variant='default'/>
-  <main>{@render children?.()}</main>
+  <main>{@render children()}
+</main>
 
     <Footer />
 </div>
