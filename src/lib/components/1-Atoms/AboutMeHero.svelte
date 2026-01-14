1bcb2efe48d1657f1ac28832073b1a1f1aced07b
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-01-14 10:46:15 +0100

    remove placeholder image

--- src/lib/components/1-Atoms/AboutMeHero.svelte
+++ src/lib/components/1-Atoms/AboutMeHero.svelte
@@ -8,7 +8,7 @@
   } from '$lib/index.js'
 export let paragraph = 'no paragraph available'
 export let heroImages;
-let placeholder = "https://plus.unsplash.com/premium_photo-1746637466037-001842a48d31?q=80&w=1567&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+
 </script>
 
 <Wrapper>
@@ -20,3 +20,4 @@ let placeholder = "https://plus.unsplash.com/premium_photo-1746637466037-001842a
 </Wrapper>
 
   <Paragraph content={paragraph}/>
+  
