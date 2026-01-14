82d244e220e60fd0df558856127cf6ab1cf2c850
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-01-14 14:43:34 +0100

    update styling of contact page

--- src/lib/components/5-Pages/Contact.svelte
+++ src/lib/components/5-Pages/Contact.svelte
@@ -10,11 +10,15 @@
 <Wrapper>
   <section>
   <HeroTitle heroTitle="Speak to you soon!" />
-  <img alt='gif' src="https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3eDhxbDd0eXY5dXkxbTZiYmx3eHhkaGNtdmpiYXBsZXhpZDl5bnZibyZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/7NoNw4pMNTvgc/giphy.gif">
-  <Hyperlink 
-  link=mailto:{email} 
-  title="{email}"
-  />
+  <div>
+    <img alt='gif' src="https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3eDhxbDd0eXY5dXkxbTZiYmx3eHhkaGNtdmpiYXBsZXhpZDl5bnZibyZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/7NoNw4pMNTvgc/giphy.gif">
+    <Hyperlink 
+    link=mailto:{email} 
+    title="{email}"
+    arrow="true"
+    />
+  </div>
+
   </section>
 </Wrapper>
 
@@ -22,5 +26,27 @@
 <style>
   section {
     min-height:100dvh;
+  }
+
+  section div {
+    display: flex;
+    gap: var(--gap-medium);
+    flex-direction:column;
+    align-items: center;
+    justify-content: center;
+    min-height:60dvh;
+
+
+    @media (min-width:700px) {
+    flex-direction: row-reverse;
+    align-items: center;
+    justify-content: space-around;
+    }
+  }
+
+  section div img {
+      display:block;
+      max-height:400px;
+      max-width:400px;
   }
 </style>
