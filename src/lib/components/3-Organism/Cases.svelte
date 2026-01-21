a8495630410bad0760e2121bf13cc2ad7651f1d5
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-01-21 19:18:04 +0100

    remove console.log

--- src/lib/components/3-Organism/Cases.svelte
+++ src/lib/components/3-Organism/Cases.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { CaseList, Hero, Hyperlinks, Wrapper } from '$lib/index.js';
 	export let caseData;
-	console.log(caseData.cases[0].workingProjects)
 </script>
 
 <Hero heroTitle="Portfolio" />
