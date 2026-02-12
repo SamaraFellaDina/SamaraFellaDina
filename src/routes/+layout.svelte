819e143d6cbd4caeb34d7ef510d3725630bf2466
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-02-12 19:48:59 +0100

    add smooth scroll animation to website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,46 @@
 <script>
 	import { page } from '$app/state';
 	import { Header, Footer } from '$lib/index.js';
-	let { data, children } = $props();
-	let footerData = data.footer;
+	
+	export let data;
+	export let children;
+
+	$: footerData = data.footer;
+
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+
+		const { gsap } = await import('gsap');
+		const { ScrollTrigger } = await import('gsap/ScrollTrigger');
+		const { ScrollSmoother } = await import('gsap/ScrollSmoother');
+
+		gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+
+		let smoother = ScrollSmoother.create({
+			wrapper: '.smooth-wrapper',
+			content: '.smooth-content'
+		})
+	});
 </script>
 
 {#if page.url.pathname === '/'}
 	<main>{@render children()}</main>
 {:else}
+		<div class='smooth-wrapper'>
+			<div class='smooth-content'>
 	<Header variant="default" />
-	<main>{@render children()}</main>
+		<main>
+				{@render children()}
+	</main>
 	<Footer {footerData} />
+				</div>
+		</div>
 {/if}
+
+<style>
+	.smooth-content {
+			overflow: hidden;
+
+	}
+</style>
